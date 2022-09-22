0a5c7b7953fbacf80daf358684b81d3e8ce915a6
Colin Stark <collinscodes@gmail.com>
2022-09-22 09:46:17 -0800

    Update tinder.sol

--- tinder.sol
+++ tinder.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.17;
 user[] public users;
 
     struct user {
-        string handle;
+        string fullname;
         string imageurl;
         uint upvotes;
         uint downvotes
+    }
+    
+    function adduser (string memory _fullname, string memory _imageurl) {
+    user.push(user(_fullname, _imageurl, 0, 0));
     }
