5b870bf482a02865ac6d1d9e8332734ad334757d
Colin Stark <collinscodes@gmail.com>
2022-09-22 09:42:58 -0800

    Update tinder.sol

--- tinder.sol
+++ tinder.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+
+user[] public users;
+
+    struct user {
+        string handle;
+        string imageurl;
+        uint upvotes;
+        uint downvotes
+    }
