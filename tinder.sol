023a720e9a33d18ce18bd88f6af51111120d6c4c
Ujayata <62179207+Ujayata@users.noreply.github.com>
2022-09-22 19:01:40 +0100

    Update tinder.sol

--- tinder.sol
+++ tinder.sol
@@ -2,15 +2,18 @@
 pragma solidity ^0.8.17;
 
 contract tinder {
-user[] public users;
+    user[] public users;
 
     struct user {
         string fullname;
         string imageurl;
+        address userwallet;
         uint upvotes;
         uint downvotes;
     }
     
     function adduser (string memory _fullname, string memory _imageurl) public {
-    users.push(user(_fullname, _imageurl, 0, 0));
-    }}
+        users.push(user(_fullname, _imageurl, (msg.sender), 0, 0));
+    }
+
+}
