d4d0a8369f401897131c9a09d26a3a8538819e9c
Ujayata <62179207+Ujayata@users.noreply.github.com>
2022-09-22 19:14:14 +0100

    Update tinder.sol

--- tinder.sol
+++ tinder.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 contract tinder {
     user[] public users;
+    mapping (address=>bool) giftaddress;
 
     struct user {
         string fullname;
@@ -13,7 +14,10 @@ contract tinder {
     }
     
     function adduser (string memory _fullname, string memory _imageurl) public {
+        require(giftaddress [msg.sender]= false,'already created an account');
         users.push(user(_fullname, _imageurl, (msg.sender), 0, 0));
+        giftaddress[msg.sender]= true;
+
     }
 
 }
